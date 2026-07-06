1c823e9f6bd4a2aaa411c5ef05dcaec5c3f2c221
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-07-06 21:48:46 +0100

    Implement FromContext and FromEvent for Context and Event respectively

--- src/lib/context.rs
+++ src/lib/context.rs
@@ -6,3 +6,9 @@ pub struct Context;
 pub trait FromContext: Send + Sync + 'static {
     fn from_ctx(event: &Context) -> Self where Self: Sized;
 }
+
+impl FromContext for Context {
+    fn from_ctx(event: &Context) -> Self {
+        event.clone()
+    }
+}
--- src/lib/event/mod.rs
+++ src/lib/event/mod.rs
@@ -6,3 +6,9 @@ pub enum Event {}
 pub trait FromEvent: Send + Sync + 'static {
     fn from_event(event: Event) -> Option<Self> where Self: Sized;
 }
+
+impl FromEvent for Event {
+    fn from_event(event: Event) -> Option<Self> {
+        Some(event)
+    }
+}
