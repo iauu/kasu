5aa23783b996a9df69c566b3e57a9c37e0879047
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-07-06 23:29:47 +0100

    Create the constructor function for event dispatcher

--- src/lib/dispatcher.rs
+++ src/lib/dispatcher.rs
@@ -2,15 +2,25 @@ use tokio::sync::broadcast::{Sender, Receiver};
 use crate::lib::context::Context;
 use crate::lib::event::Event;
 
+#[derive(Clone)]
 pub struct EventDispatcher {
     tx: Sender<(Event, Context)>
 }
 
+
+
 impl EventDispatcher {
+    pub fn new(size: usize) -> Self {
+        let (tx, _) = tokio::sync::broadcast::channel(size);
+        Self {
+            tx
+        }
+    }
+
     fn send(&self, event: Event, context: Context) -> () {
         self.tx.send((event, context)).unwrap();
     }
-    
+
     fn subscribe(&self) -> Receiver<(Event, Context)> {
         self.tx.subscribe()
     }
