2986aa90cfbf4a32c4a9c846343b444706956a98
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-07-12 00:35:33 +0100

    Fix time :p

--- src/tasks/pfp_task.rs
+++ src/tasks/pfp_task.rs
@@ -5,7 +5,7 @@ use crate::state::{BotState, Profile};
 
 pub async fn pfp_task(partial_client: PartialClient, state: BotState) -> ! {
     loop {
-        if (state.read().await.current_pfp == Profile::Katie && state.read().await.last_message.elapsed().as_secs() > 30) {
+        if (state.read().await.current_pfp == Profile::Katie && state.read().await.last_message.elapsed().as_secs() > 300) {
             state.write().await.current_pfp = Profile::Shy;
             fail_ignore_handle!(partial_client.read().await.api_client.set_profile(Profile::Shy.as_path()).await, "Reset pfp failed {e}");
         }
