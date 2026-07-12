a8f3639df66caba77e8de6e50a3eb33fe2735ba9
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-07-12 22:38:24 +0100

    Implement optional parse

--- src/lib/cmd/parser/mod.rs
+++ src/lib/cmd/parser/mod.rs
@@ -1,5 +1,18 @@
+use nom::IResult;
+use crate::lib::cmd::parse::Parse;
+
 mod quoted;
 mod string;
 mod user;
 mod channel;
 mod ts;
+
+impl<X: Parse> Parse for Option<X> {
+    fn parse(arg: &str) -> IResult<&str, Self> {
+        let res = X::parse(arg);
+        Ok(match res {
+            Ok(r) => (r.0, Some(r.1)),
+            Err(_) => (arg, None)
+        })
+    }
+}
